6dc83531501f9f4601dd61a448fa3f2f1a56c249
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-02-12 19:54:41 -0600

    Add Google analytics

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App';
 import router from './router';
 import store from './store';
 import vueSocketio from 'vue-socket.io';
+import VueAnalytics from 'vue-analytics'
 
 import 'bulma';
 import 'font-awesome/css/font-awesome.min.css';
@@ -12,6 +13,11 @@ import 'font-awesome/css/font-awesome.min.css';
 Raven.config('https://f7fb18c3c7194c9589192cc0aab51c84@sentry.io/287017')
   .addPlugin(RavenVue, Vue)
   .install();
+
+Vue.use(VueAnalytics, {
+  id: 'UA-49467961-4',
+  router
+});
 
 Vue.config.productionTip = false;
 
